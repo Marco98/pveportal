3362e1d0314ec79ce8e31b5ec22e4d7440dc9738
Marco98 <Marco98@users.noreply.github.com>
2024-07-26 12:32:53 +0200

    feat: add post-install and -remove scripts

---
+++ res/postinstall.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+UNITNAME=pveportal.service
+
+cleanInstall() {
+	echo "Runinng clean postinstall:"
+	echo "  Reload systemd service unit file"
+	systemctl daemon-reload ||:
+	echo "  Unmask systemd service unit"
+	systemctl unmask $UNITNAME ||:
+	echo "  Set the preset flag for systemd service unit"
+	systemctl preset $UNITNAME ||:
+	echo "  Set the enabled flag for the service unit"
+	systemctl enable $UNITNAME ||:
+	echo "  Start systemd service unit"
+	systemctl restart $UNITNAME ||:
+}
+
+upgrade() {
+	echo "Running upgrade postinstall:"
+	echo "  Reload systemd service unit file"
+	systemctl daemon-reload ||:
+	echo "  Start/Restart systemd service unit"
+	systemctl try-restart $UNITNAME ||:
+	systemctl is-enabled $UNITNAME && systemctl start $UNITNAME ||:
+}
+
+action="$1"
+if [ "$1" = "configure" ] && [ -z "$2" ]; then
+	action="install"
+elif [ "$1" = "configure" ] && [ -n "$2" ]; then
+	action="upgrade"
+fi
+
+case "$action" in
+	"1" | "install")
+		cleanInstall
+	;;
+	"2" | "upgrade")
+		upgrade
+	;;
+	*)
+		cleanInstall
+	;;
+esac
---
+++ res/postremove.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+UNITNAME=pveportal.service
+
+remove() {
+	echo "Running normal postremove:"
+	echo "  Stop systemd service unit"
+	systemctl stop $UNITNAME ||:
+	echo "  Reload systemd service unit file"
+	systemctl daemon-reload ||:
+}
+
+purge() {
+	echo "Running purge postremove:"
+}
+
+upgrade() {
+	echo "Running upgrade postremove:"
+}
+
+action="$1"
+case "$action" in
+	"0" | "remove")
+		remove
+	;;
+	"1" | "upgrade")
+		upgrade
+	;;
+	"purge")
+		purge
+	;;
+	*)
+		remove
+	;;
+esac
