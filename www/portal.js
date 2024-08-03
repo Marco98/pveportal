1b2417e6e98fa7b44154bd4b2f1069fb0817eb75
Marco98 <Marco98@users.noreply.github.com>
2024-08-04 00:26:16 +0200

    fix: undisturb extjs by overlaying clusterlist

--- www/portal.js
+++ www/portal.js
@@ -13,55 +13,74 @@ const xhr = new XMLHttpRequest();
 xhr.open("GET", "/pveportal/api/clusters");
 xhr.send();
 xhr.responseType = "json";
-xhr.onload = () => {
-  waitForElement("#treeview-1013", () => {
-    for (var i = 0; i < xhr.response.clusters.length; i++) {
-      document.getElementById("treeview-1013").innerHTML += `<a
-      href="${xhr.response.clusters[i].switch_url}"
-      style="text-decoration:none;">
-      <div
-      class="x-grid-item-container"
+xhr.onload = () => waitForElement("#treeview-1013", createPvePortalClusters);
+
+function createPvePortalClusters() {
+  let clusters = document.createElement("div");
+  clusters.setAttribute("id", "pveportal-clusters");
+  clusters.style.position = "absolute";
+  clusters.style.zIndex = 99;
+  clusters.style.top = "0px";
+  clusters.style.left = "0px";
+  for (var i = 0; i < xhr.response.clusters.length; i++) {
+    clusters.innerHTML += `<a
+    href="${xhr.response.clusters[i].switch_url}"
+    style="text-decoration:none;">
+    <table
       role="presentation"
-      style="width: 298px; transform: translate3d(0px, 0px, 0px)"
-      >
-      <table
-        role="presentation"
-        class="x-grid-item"
-        cellpadding="0"
-        cellspacing="0"
-        style="width: 0;padding-left: 16px"
-      >
-        <tbody>
-          <tr
-            class="x-grid-tree-node-expanded x-grid-row"
-            role="row"
-            data-qtip=""
-            data-qtitle=""
-            aria-level="1"
-            aria-expanded="true"
+      class="x-grid-item"
+      cellpadding="0"
+      cellspacing="0"
+      style="padding-left: 16px"
+    >
+    <div
+    role="presentation"
+    style="transform: translate3d(0px, 0px, 0px)"
+    >
+      <tbody>
+        <tr
+          class="x-grid-row"
+          role="row"
+          aria-level="1"
+          aria-expanded="true"
+        >
+          <td
+            class="x-grid-td"
+            role="gridcell"
+            tabindex="-1"
           >
-            <td
-              class="x-grid-cell x-grid-td x-grid-cell-treecolumn x-grid-cell-first x-grid-cell-last x-unselectable"
-              style="width: 298px"
-              role="gridcell"
-              tabindex="-1"
+            <div
+              class="x-grid-cell-inner x-grid-cell-inner-treecolumn"
+              style="text-align: left"
             >
               <div
-                unselectable="on"
-                class="x-grid-cell-inner x-grid-cell-inner-treecolumn"
-                style="text-align: left"
-              >
-                <div
-                  role="presentation"
-                  class="x-tree-icon x-tree-icon-custom x-tree-icon-parent-expanded fa fa-server"
-                ></div>
-                <span class="x-tree-node-text">Cluster ${xhr.response.clusters[i].name}</span>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      </div></a>`;
-    }
-  });
-};
+                role="presentation"
+                class="x-tree-icon x-tree-icon-custom x-tree-icon-parent-expanded fa fa-server"
+              ></div>
+              <span class="x-tree-node-text">Cluster ${xhr.response.clusters[i].name}</span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div></a>`;
+  }
+  document.body.appendChild(clusters);
+  updatePvePortalClustersLoc();
+  let obs = new MutationObserver(updatePvePortalClustersLoc);
+  obs.observe(
+    document
+      .getElementById("treeview-1013")
+      .getElementsByClassName("x-grid-item-container")[0],
+    { attributes: true, childList: true, subtree: true }
+  );
+}
+
+function updatePvePortalClustersLoc() {
+  let rect = document
+    .getElementById("treeview-1013")
+    .getElementsByClassName("x-grid-item-container")[0]
+    .getBoundingClientRect();
+  document.getElementById("pveportal-clusters").style.top = rect.bottom + "px";
+  document.getElementById("pveportal-clusters").style.left = rect.left + "px";
+}
